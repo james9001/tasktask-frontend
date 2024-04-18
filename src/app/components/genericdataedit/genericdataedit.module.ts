f5f4caf2e61b5fa795785e552a2c4ff966bf06c7
james9001 <james9001@james9001.net>
2024-04-18 21:10:03 +1000

    Delete redundant and pointless routing module

--- src/app/components/genericdataedit/genericdataedit.module.ts
+++ src/app/components/genericdataedit/genericdataedit.module.ts
@@ -4,12 +4,10 @@ import { FormsModule } from "@angular/forms";
 
 import { IonicModule } from "@ionic/angular";
 
-import { GenericdataeditPageRoutingModule } from "./genericdataedit-routing.module";
-
 import { GenericdataeditPage } from "./genericdataedit.page";
 
 @NgModule({
-	imports: [CommonModule, FormsModule, IonicModule, GenericdataeditPageRoutingModule],
+	imports: [CommonModule, FormsModule, IonicModule],
 	declarations: [GenericdataeditPage],
 })
 export class GenericdataeditPageModule {}
